610b5d28d3ac4ccd58de8fa9abdace98e375dfa2
Minh Doan <daywednes@gmail.com>
2021-09-30 23:52:50 +0700

    Update Rena.sol
    Remove inheritance of ERC20 and import of ERC20.

--- contracts/Rena.sol
+++ contracts/Rena.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract WarenaToken is ERC20, ERC20Burnable {
+contract WarenaToken is ERC20Burnable {
     constructor() ERC20("Warena Token", "RENA") {
         _mint(msg.sender, 100000000 * 10 ** decimals());
     }
